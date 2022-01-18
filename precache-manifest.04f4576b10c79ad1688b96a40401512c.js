d3b42235ecef5f554576a70eb8d6c86c408c8855
haithamassoli <haitham.b.assoli@gmail.com>
2022-01-18 11:00:42 +0200

    Updates

---
+++ precache-manifest.04f4576b10c79ad1688b96a40401512c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a30563415536633e4988af41df1d846",
+    "url": "/Review-Page-React/index.html"
+  },
+  {
+    "revision": "31ec0c9590bf53aa73e8",
+    "url": "/Review-Page-React/static/css/main.684b11a9.chunk.css"
+  },
+  {
+    "revision": "be19f08927a2fa0493c2",
+    "url": "/Review-Page-React/static/js/2.fcbd3efb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Review-Page-React/static/js/2.fcbd3efb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31ec0c9590bf53aa73e8",
+    "url": "/Review-Page-React/static/js/main.9609f434.chunk.js"
+  },
+  {
+    "revision": "c8e94747a3e3ae09c7d8",
+    "url": "/Review-Page-React/static/js/runtime-main.e83a2676.js"
+  }
+]);
